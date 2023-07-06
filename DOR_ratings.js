51895827f0412d761b58b5f0fb90720d9b28e43f
FabiLSRP <138776390+FabiLSRP@users.noreply.github.com>
2023-07-06 16:12:20 +0300

    Create DOR_ratings.js

---
+++ DOR_ratings.js
@@ -0,0 +1,29 @@
+{
+    "ATTITUDE": [
+        "1. Acceptance of Feedback",
+        "2. Relationship with Citizens",
+        "3. Appearance",
+        "4. Leadership"
+    ],
+    "PERFORMANCE": [
+        "4.	Driving Skill",
+        "5.	Field Performance",
+        "6.	Investigative Skills",
+        "7.	Interview Skills",
+        "8.	Officer Safety",
+        "9.	Investigative Skills",
+        "10.	Problem Solving",
+        "11.	Vehicle/Pedestrian Stops",
+        "12.	Radio Communication"
+    ],
+    "KNOWLEDGE": [
+        "13.	Department Policy",
+        "14.	Criminal Law",
+        "15.	Criminal Procedure"
+    ],
+    "(( OUT OF CHARACTER ))": [
+        "16.	Overall Mindset",
+        "17.	Character Portrayal",
+        "18.	Passive Roleplay"
+    ]
+}
